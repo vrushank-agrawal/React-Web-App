9b93fde18da6bb22badfec043620a6e1879d7151
vrushank-agrawal <vrushank2001@gmail.com>
2023-05-13 12:20:38 +0200

    rearrange Dashboard

---
+++ src/pages/Dashboard/SinistresTable.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { List, ListItem, Paper, Typography, Grid, Divider } from "@mui/material";
+
+import { ORANGEDARK, ORANGELIGHT } from "../../utils/colors";
+import CodenektButton from "../../Components/CodeNektButton";
+
+const SinistreListItem = (props) => {
+    return (
+        <List disablePadding sx={{padding: "0"}}>
+            <ListItem sx={{padding: "0 0 0.2rem 1rem"}}> {props.XX} </ListItem>
+            <ListItem sx={{padding: "0 0 0.2rem 1rem"}}> {props.Date} </ListItem>
+            <ListItem sx={{padding: "0 0 0.2rem 1rem"}}> {props.statut} </ListItem>
+            <ListItem sx={{padding: "0 0 0.2rem 1rem", color: ORANGEDARK, textDecoration: 'underline' }}> {props.link} </ListItem>
+        </List>
+    )
+}
+
+const SinistreButton = () => {
+    return (
+        <CodenektButton
+            blue={true}
+            bold={true}
+            height={"fit-content"}
+            margin={"auto"}
+            padding={"0.5rem"}
+            title={"VOIR TOUS LES SINISTRES"}
+            width={"10rem"}
+        />
+    )
+}
+
+const SinistreTable = () => {
+    return (
+        <Paper elevation={3} style={{ width: "100%", height: "100%", paddingBottom: "1rem" }}>
+            <Typography variant="h6" style={{ fontWeight: "bold", padding: "1rem" }}> SINISTRES </Typography>
+            <Divider sx={{ backgroundColor: ORANGELIGHT }} />
+            <Grid container spacing={2} alignItems="stretch" style={{ marginTop: "0.5rem", justify: "center", marginBottom: "1rem" }}>
+                <Grid item xs={12} sm={2} md={2}>
+                    <SinistreListItem XX="XX" Date="Date" statut="Statut" link="CONSULTER" />
+                </Grid>
+                <Grid item xs={12} sm={2} md={2}>
+                    <SinistreListItem XX="XX" Date="Date" statut="Statut" link="CONSULTER" />
+                </Grid>
+                <Grid item xs={12} sm={2} md={2}>
+                    <SinistreListItem XX="XX" Date="Date" statut="Statut" link="CONSULTER" />
+                </Grid>
+                <Grid item xs={12} sm={2} md={2}>
+                    <SinistreListItem XX="XX" Date="Date" statut="Statut" link="CONSULTER" />
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} sx={{alignSelf: "flex-end"}}>
+                    <SinistreButton />
+                </Grid>
+            </Grid>
+        </Paper>
+    )
+}
+
+export default SinistreTable;
---
+++ src/pages/Dashboard/VehiclesTable.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { Paper, Typography, Grid, Divider, Chip } from '@mui/material';
+
+// Local imports
+import { FONTSEMIBIG } from '../../utils/fontSize';
+import { BLACKCN, BLUECN, ORANGEDARK, ORANGELIGHT, WHITECN, GREENCN } from '../../utils/colors';
+import CodenektButton from '../../Components/CodeNektButton';
+
+const VehicleTableValueSize = "1.2rem";
+const VehicleTableFontsize = "14px";
+
+const VehicleChip = (props) => {
+    return (
+        <Chip
+            label={props.label}
+            variant={(props.defvar) ? props.variant : "filled"}
+            sx={{fontWeight: "bold",
+                padding: "0.2rem",
+                fontSize: props.fontSize,
+                backgroundColor: props.backgroundColor,
+                color: props.color,
+                width: "fit-content",
+            }}
+        />
+    )
+}
+
+const VehicleTypography = (props) => {
+    return (
+        <Typography variant="h6"
+            style={{
+                marginLeft: "1rem",
+                fontSize: VehicleTableFontsize,
+                fontWeight: "bold",
+                padding: "0.2rem",
+            }}
+        >
+            {props.title}
+        </Typography>
+    )
+}
+
+const VehiclesButton = () => {
+    return (
+        <CodenektButton
+            title={"VOIR TOUS LES VÉHICULES"}
+            width={"fit-content"}
+            bold={true}
+            blue={true}
+            padding={"0.5rem"}
+            margin={"auto"}
+            height={"fit-content"}
+        />
+    )
+}
+
+const VehiclesTable = () => {
+    return (
+        <Paper elevation={3}
+            style={{ width: "100%", height: "100%", paddingBottom: "1rem",
+            display: "flex", justifyContent: "center", flexDirection: "column"
+        }}>
+            <Typography variant="h6" style={{ fontWeight: "bold", padding: "1rem", alignContent: "left", }}> Véhicules </Typography>
+            <Divider sx={{ backgroundColor: ORANGELIGHT }} />
+            <Chip
+                label="33"
+                variant="outlined"
+                sx={{
+                    margin: "auto",
+                    marginTop: "1rem",
+                    fontWeight: "bold",
+                    padding: "0.2rem",
+                    fontSize: FONTSEMIBIG,
+                    color: BLACKCN,
+                    width: "fit-content",
+                    borderColor: ORANGELIGHT,
+                }}
+            />
+            <Grid container spacing={2} style={{ marginTop: "0.5rem", justify: "center", marginBottom: "1rem" }}>
+                <Grid item xs={12} sm={6} md={8}>
+                    <VehicleTypography title="EN COMMANDE" />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <VehicleChip
+                        label="30"
+                        fontSize={VehicleTableValueSize}
+                        backgroundColor={BLUECN}
+                        color={WHITECN}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={8}>
+                    <VehicleTypography title="AFFECTÉS" />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <VehicleChip
+                        label="2"
+                        fontSize={VehicleTableValueSize}
+                        backgroundColor={GREENCN}
+                        color={WHITECN}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={8}>
+                    <VehicleTypography title="LIBRES" />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <VehicleChip
+                        label="1"
+                        fontSize={VehicleTableValueSize}
+                        backgroundColor={ORANGEDARK}
+                        color={WHITECN}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={8}>
+                    <VehicleTypography title="EN ATTENTE DE VALIDATION" />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <VehicleChip
+                        label="1"
+                        fontSize={VehicleTableValueSize}
+                        backgroundColor={ORANGEDARK}
+                        color={WHITECN}
+                    />
+                </Grid>
+            </Grid>
+            <VehiclesButton />
+        </Paper>
+    )
+}
+
+export default VehiclesTable;
