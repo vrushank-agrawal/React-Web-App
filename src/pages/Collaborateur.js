e70ee9d3e44a7623c498bda5bd813a3b823eaae9
vrushank-agrawal <vrushank2001@gmail.com>
2023-05-22 00:14:19 +0200

    add Ajouter Collaborateur link to collaborateur page

--- src/pages/Collaborateur.js
+++ src/pages/Collaborateur.js
@@ -1,17 +1,15 @@
 import React from "react";
-import CodeNektTable from "../Components/CodeNektTable";
-import CodenektButton from "../Components/CodeNektButton";
+import { Grid, Stack } from "@mui/material";
 
 // Local imports
+import CodeNektTable from "../Components/CodeNektTable";
+import CodenektButton from "../Components/CodeNektButton";
 import { collaborator as data } from "./utils/test-data";
-import { FONTSEMIBIG } from "../utils/fontSize";
-import { FONTBOLD } from "../utils/fonts";
-
-// Material UI imports
+import { FONTSEMIBIG, LITTLE2 } from "../utils/fontSize";
+import { FONTBOLD } from "../utils/fonts"
 import { BLACKCN, WHITECN } from "../utils/colors";
-import { Stack } from "@mui/material";
-import { CodeNektEye } from "../Components/CodeNektIcons";
-import { LinkVoirCollab } from "../Components/CodeNektPageLinks";
+import { CodeNektEye, CodeNektAdd } from "../Components/CodeNektIcons";
+import { LinkAjoutCollab, LinkVoirCollab } from "../Components/CodeNektPageLinks";
 
 const column = {
     names: [
@@ -53,9 +51,22 @@ const column = {
 function Collaborator() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'left' }}>
-        <h1 size={FONTSEMIBIG} style={{fontWeight: FONTBOLD}}>
-            Collaborateur
-        </h1>
+        <Grid container spacing={2} direction={"row"}>
+            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+                <h1 size={FONTSEMIBIG} style={{fontWeight: FONTBOLD}}>
+                    {"liste des collaborateurs".toLocaleUpperCase()}
+                </h1>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} lg={6} xl={6} style={{display: "flex", justifyContent: "right"}}>
+                <CodenektButton blue
+                    color={WHITECN}
+                    size={LITTLE2}
+                    startIcon={<CodeNektAdd size={15} />}
+                    title={"Ajouter un collaborateur"}
+                    to={LinkAjoutCollab}
+                />
+            </Grid>
+        </Grid>
         <CodeNektTable
             column={column}
             data={data}
